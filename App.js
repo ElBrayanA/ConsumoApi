072e06b80d47098282e67fdfd74b55636ab0c399
ElBrayanA <ffrfdke86@gmail.com>
2024-10-22 17:25:34 -0700

    Consumo de api pokemon

--- App.js
+++ App.js
@@ -1,13 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { FlatList, Modal, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      loading: false,
+      pokemon:[],
+      url: 'https://pokeapi.co/api/v2/pokemon'
+    }
+  }
+  componentDidMount(){
+    this.getPokemon();
+  }
+
+  getPokemon = () =>{
+    this.setState({loading:true})
+
+    fetch(this.state.url)
+    .then(res => res.json())
+    .then(res =>{
+      this.setState({
+        pokemon: res.results,
+        url: res.next,
+        loading: false
+      })
+    });
+  };
+
+render(){
+  if(this.state.loading){
+    return(
+      <View style={ styles.container}>
+        <Text>Descargando Pokemon</Text>
+      </View>
+    );
+  }
+
+  return(
+    <View style={{flex:1, paddingTop:50, paddingLeft:5}}>
+      <FlatList
+      data={this.state.pokemon}
+      renderItem={({item}) => 
+        <Text>{item.name}</Text>
+    }
+      />
     </View>
-  );
+  )
+  
+}
 }
 
 const styles = StyleSheet.create({
